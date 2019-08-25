4db3d0a60dbbce554469d60a95f4417f44464e4b
EugeneZakhno <30446748+EugeneZakhno@users.noreply.github.com>
2019-08-25 23:24:17 +0300

    Update BinarySearch.java

--- 01_introduction_to_algorithms/java/01_binary_search/src/BinarySearch.java
+++ 01_introduction_to_algorithms/java/01_binary_search/src/BinarySearch.java
@@ -24,7 +24,21 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] myList = {1, 3, 5, 7, 9};
 
-        System.out.println(binarySearch(myList, 3)); // 1
-        System.out.println(binarySearch(myList, -1)); // null
+        System.out.println(binarySearch(myList, -2)); // index null
+        System.out.println(binarySearch(myList, -1)); //index null
+        System.out.println(binarySearch(myList, 0)); //index null
+        System.out.println(binarySearch(myList, 1)); //index 0
+        System.out.println(binarySearch(myList, 2)); //index null
+        System.out.println(binarySearch(myList, 3)); //index 1
+        System.out.println(binarySearch(myList, 4)); //index null
+        System.out.println(binarySearch(myList, 5)); //index 2
+        System.out.println(binarySearch(myList, 6)); //index null
+        System.out.println(binarySearch(myList, 7));//index 3
+        System.out.println(binarySearch(myList, 8));//index null
+        System.out.println(binarySearch(myList, 9));//index 4
+        System.out.println(binarySearch(myList, 10));//index null
+        System.out.println(binarySearch(myList, 11));//index null
+        System.out.println(binarySearch(myList, 12));//index null
+        System.out.println(binarySearch(myList, 13));//index null
     }
 }
